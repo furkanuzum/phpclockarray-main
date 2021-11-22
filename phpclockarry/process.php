b4cc7dc4403f338d6877589feb3f423a4c40c8b5
furkanuzum <furkanuzum61@gmail.com>
2021-11-22 13:34:42 +0300

    phpcase2

--- phpclockarry/process.php
+++ phpclockarry/process.php
@@ -1,64 +1,51 @@
-<?PHP
-    function printArray($array){
-      //print_r($array);
-      $length = count($array);
-      for($i=0;$i<$length;$i++){
-        $length2= count($array[$i]);
-        for($j=0;$j<$length2;$j++){
-          $myNumber=str_pad($array[$i][$j],2," ",STR_PAD_LEFT);
-          print_r($myNumber."-");
-        }
-        
-        print_r("\n");
-      }
+<?php
+
+function printArray($array)
+{
+  $length = count($array);
+  for ($i = 0; $i < $length; $i++) {
+    $length2 = count($array[$i]);
+    for ($j = 0; $j < $length2; $j++) {
+      $myNumber=$array[$j][$i];
+      echo $myNumber . "\t";
     }
-    function solution1(){
-      $x=10;
-      $y=8;
-      $myArray=array();//boş matris
-      $outerFrame=function ($matris,$step,$startNumber) use($x,$y)//step kaçıncı çerçeve
-      {
-          $startX=$step;
-          $endX=$x-$step-1; 
+    echo "\n";
+  }
+}
 
-          $startY=$step;
-          $endY=$y-$step-1;
-          for($i=$startX;$i<=$endX;$i++){
-              $matris[$i][$startY]=$startNumber;
-              $startNumber++;
-          }
-          for($i=$startY+1;$i<=$endY;$i++){
-              $matris[$endX][$i]=$startNumber;
-              $startNumber++;
-          }
-          for($i=$endX-1;$i>=$startX;$i--){
-              $matris[$i][$endY]=$startNumber;
-              $startNumber++;
-          }
-          for($i=$endY-1;$i>=$startY+1;$i--){
-              $matris[$startX][$i]=$startNumber;
-              $startNumber++;
-          }
-          //print "step: $step";
-          //print_r($matris);
-          return [$matris,$startNumber];
-      };
-      $myStartNumber=0;
-      $minimum=min($x,$y); 
-      $frameCount=ceil($minimum/2);//çerçeve sayısı belırle
-      for($i=0;$i<=$frameCount;$i++){
-        [$myArray,$myStartNumber]=$outerFrame($myArray,$i,$myStartNumber);
-      }
-      printArray($myArray);
+  $x = 10;
+  $y = 10;
+  $myArray = array(); //boş matris
+  $outerFrame = function ($matris, $step, $startNumber) use ($x, $y) //step kaçıncı çerçeve
+  {
+    $startX = $step;
+    $endX = $x - $step - 1;
+
+    $startY = $step;
+    $endY = $y - $step - 1;
+    for ($i = $startX; $i <= $endX; $i++) {
+      $matris[$i][$startY] = $startNumber;
+      $startNumber++;
     }
-    solution1();
-
-
-    function solution2(){
-
+    for ($i = $startY + 1; $i <= $endY; $i++) {
+      $matris[$endX][$i] = $startNumber;
+      $startNumber++;
     }
-    solution2();
-
-
-
+    for ($i = $endX - 1; $i >= $startX; $i--) {
+      $matris[$i][$endY] = $startNumber;
+      $startNumber++;
+    }
+    for ($i = $endY - 1; $i >= $startY + 1; $i--) {
+      $matris[$startX][$i] = $startNumber;
+      $startNumber++;
+    }
+    return [$matris, $startNumber];
+  };
+  $myStartNumber = 0;
+  $maximum = max($x, $y);
+  $frameCount = ceil($maximum); //çerçeve sayısı belırle
+  for ($i = 0; $i <= $frameCount; $i++) {
+    [$myArray, $myStartNumber] = $outerFrame($myArray, $i, $myStartNumber);
+  }
+  printArray($myArray);
 ?>
